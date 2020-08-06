0b03d58afb0d8934177e172e8d160e7594710a5a
Agustin <agustin.gambirassi@hasar.com>
2020-08-06 19:52:12 -0300

    Fix typo en MutantServiceTest

--- src/test/java/com/code/challenge/service/MutantServiceTest.java
+++ src/test/java/com/code/challenge/service/MutantServiceTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Agustin Gambirassi
  **/
 @SpringBootTest
-public class MutanServiceTest {
+public class MutantServiceTest {
 
     @Autowired
     private MutantService mutantService;
